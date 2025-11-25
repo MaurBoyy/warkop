f4e29ddb72aa2648270e0aeb47d86b69d100dd03
MaurBoyy <148849733+MaurBoyy@users.noreply.github.com>
2025-11-26 05:22:59 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        alansans: ["Alan Sans", "sans-serif"],
+        urbanist: ["Urbanist", "sans-serif"], // ⬅️ tambahin ini
+      },
+    },
+  },
+  plugins: [],
+};
